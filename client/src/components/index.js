8247e5ead0805ecd5cb4efd7ce241b4c16cfd987
alext111 <49249379+alext111@users.noreply.github.com>
2021-09-18 18:06:26 -0400

    remove front end

--- client/src/components/index.js
+++
@@ -1,4 +0,0 @@
-import Links from './links'
-import NavBar from './navbar'
-
-export { Links, NavBar }
--- client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import LoginsCreate from "./LoginsCreate"
-import LoginsFindByWebsite from "./LoginsFindByWebsite"
-import LoginsFindAll from "./LoginsFindAll"
-import LoginsUpdate from "./LoginsUpdate"
-
-export { LoginsCreate, LoginsFindByWebsite, LoginsFindAll, LoginsUpdate }
