6316e3648633dacb728d9437b94c3838a2892b95
alext111 <49249379+alext111@users.noreply.github.com>
2022-02-24 17:53:55 -0500

    url handling

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, '../client/build')))
 
 app.use('/api', urlRouter)
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/../client/build/index.html'))
   })
 
